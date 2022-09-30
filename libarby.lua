5d8e9e6a940171604c2004d95033108d131161c9
cooldarksouktaker <64041569+cooldarksouktaker@users.noreply.github.com>
2022-09-30 18:07:52 -0500

    Update libarby.lua

--- libarby.lua
+++ libarby.lua
@@ -6,7 +6,7 @@ local Mouse = game.Players.LocalPlayer:GetMouse()
 
 local Blacklist = {Enum.KeyCode.Unknown, Enum.KeyCode.CapsLock, Enum.KeyCode.Escape, Enum.KeyCode.Tab, Enum.KeyCode.Return, Enum.KeyCode.Backspace, Enum.KeyCode.Space, Enum.KeyCode.W, Enum.KeyCode.A, Enum.KeyCode.S, Enum.KeyCode.D}
 
-if CoreGui:FindFirstChild("libary") then
+if CoreGui:FindFirstChild("library") then
     CoreGui.libray:Destroy()
     CoreGui.Tooltips:Destroy()
 end
@@ -52,7 +52,7 @@ local getcustomasset = getcustomasset or getsynasset
 local isfolder = isfolder or syn_isfolder or is_folder
 local makefolder = makefolder or make_folder or createfolder or create_folder
 
-if not isfolder("libary") then
+if not isfolder("library") then
 local download = Instance.new("ScreenGui")
 download.Name = "Download"
 download.Enabled = true
@@ -123,34 +123,34 @@ dText.Position = UDim2.new(0.00132, 0, 0.39, 0)
 dText.Size = UDim2.new(0, 284, 0, 46)
 dText.Parent = dMain
     
-makefolder("libary")
+makefolder("library")
     
 local Circle = request({Url = "https://raw.githubusercontent.com/Rain-Design/Icons/main/Circle.png", Method = "GET"})
-writefile("libary/Circle.png", Circle.Body)
+writefile("library/Circle.png", Circle.Body)
 dText.Text = "Downloaded: Circle.png"
     
 local ColorDropper = request({Url = "https://raw.githubusercontent.com/Rain-Design/Icons/main/ColorDropper.png", Method = "GET"})
-writefile("libary/ColorDropper.png", ColorDropper.Body)
+writefile("library/ColorDropper.png", ColorDropper.Body)
 dText.Text = "Downloaded: ColorDropper.png"
 
 local Close = request({Url = "https://raw.githubusercontent.com/Rain-Design/Icons/main/Close.png", Method = "GET"})
-writefile("libary/Close.png", Close.Body)
+writefile("library/Close.png", Close.Body)
 dText.Text = "Downloaded: Close.png"
 
 local CollapseArrow = request({Url = "https://raw.githubusercontent.com/Rain-Design/Icons/main/CollapseArrow.png", Method = "GET"})
-writefile("lybary/CollapseArrow.png", CollapseArrow.Body)
+writefile("library/CollapseArrow.png", CollapseArrow.Body)
 dText.Text = "Downloaded: CollapseArrow.png"
     
 local RadioButton = request({Url = "https://raw.githubusercontent.com/Rain-Design/Icons/main/RadioButton.png", Method = "GET"})
-writefile("libary/RadioButton.png", RadioButton.Body)
+writefile("library/RadioButton.png", RadioButton.Body)
 dText.Text = "Downloaded: RadioButton.png"
     
 local RadioOuter = request({Url = "https://raw.githubusercontent.com/Rain-Design/Icons/main/RadioOuter.png", Method = "GET"})
-writefile("lybary/RadioOuter.png", RadioOuter.Body)
+writefile("library/RadioOuter.png", RadioOuter.Body)
 dText.Text = "Downloaded: RadioOuter.png"
     
 local RadioInner = request({Url = "https://raw.githubusercontent.com/Rain-Design/Icons/main/RadioInner.png", Method = "GET"})
-writefile("libary/RadioInner.png", RadioInner.Body)
+writefile("library/RadioInner.png", RadioInner.Body)
 dText.Text = "Downloaded: RadioInner.png"
 
 download:Destroy()
@@ -163,12 +163,12 @@ function library:GetXY(GuiObject)
 end
 
 function library:Window(Info)
-Info.Text = Info.Text or "libary"
+Info.Text = Info.Text or "library"
 
 local window = {}
 
 local librayScreenGui = Instance.new("ScreenGui")
-libraryScreenGui.Name = "libary"
+libraryScreenGui.Name = "library"
 libaryScreenGui.Parent = CoreGui
 
 local tooltipScreenGui = Instance.new("ScreenGui")
@@ -252,7 +252,7 @@ main.BorderSizePixel = 0
 main.ClipsDescendants = true
 main.Position = UDim2.new(0.361, 0, 0.308, 0)
 main.Size = UDim2.new(0, 450, 0, 321)
-main.Parent = libaryScreenGui
+main.Parent = libraryScreenGui
 
 local uICorner = Instance.new("UICorner")
 uICorner.Name = "UICorner"
@@ -1590,7 +1590,7 @@ radioButtonText.Parent = button
 
 local radioButtonIcon = Instance.new("ImageLabel")
 radioButtonIcon.Name = "RadioButtonIcon"
-radioButtonIcon.Image = getcustomasset("libary/CollapseArrow.png")
+radioButtonIcon.Image = getcustomasset("library/CollapseArrow.png")
 radioButtonIcon.AnchorPoint = Vector2.new(1, 0)
 radioButtonIcon.ImageColor3 = Color3.fromRGB(191, 191, 191)
 radioButtonIcon.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
@@ -1603,7 +1603,7 @@ radioButtonIcon.Parent = button
 
 local radioButtonIcon2 = Instance.new("ImageLabel")
 radioButtonIcon2.Name = "RadioButtonIcon2"
-radioButtonIcon2.Image = getcustomasset("libary/RadioButton.png")
+radioButtonIcon2.Image = getcustomasset("library/RadioButton.png")
 radioButtonIcon2.AnchorPoint = Vector2.new(1, 0)
 radioButtonIcon2.ImageColor3 = Color3.fromRGB(191, 191, 191)
 radioButtonIcon2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
@@ -1656,7 +1656,7 @@ radioTextButton.Parent = radio
 
 local radioOuter = Instance.new("ImageLabel")
 radioOuter.Name = "RadioOuter"
-radioOuter.Image = getcustomasset("libary/RadioOuter.png")
+radioOuter.Image = getcustomasset("library/RadioOuter.png")
 radioOuter.ImageColor3 = Color3.fromRGB(191, 191, 191)
 radioOuter.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 radioOuter.BackgroundTransparency = 1
@@ -1667,7 +1667,7 @@ radioOuter.Parent = radio
 
 local radioInner = Instance.new("ImageLabel")
 radioInner.Name = "RadioInner"
-radioInner.Image = getcustomasset("libary/RadioInner.png")
+radioInner.Image = getcustomasset("library/RadioInner.png")
 radioInner.AnchorPoint = Vector2.new(0.5, 0.5)
 radioInner.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 radioInner.BackgroundTransparency = 1
